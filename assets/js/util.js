f21a7208dca75490f3020431733e03dbb707c34a
ndo@ndo3 <yo@ni.co.de>
2019-07-09 22:21:44 +0200

    major update

---
+++ assets/js/util.js
@@ -0,0 +1,18 @@
+new Typed('#typed1', {
+  strings: ["Nico Domino"],
+  typeSpeed: 80,
+  startDelay: 500,
+  showCursor: false
+});
+
+new Typed('#typed2', {
+  strings: ["I am a Web Developer", "I am a Sys Admin", "I am a designer",  "I am a photographer", "I am a human :)"],
+  typeSpeed: 80,
+  startDelay: 1200,
+  showCursor: true,
+  /* cursorChar: '|', */
+  smartBackspace: true,
+  loop: true,
+  loopCount: 3
+});
+
